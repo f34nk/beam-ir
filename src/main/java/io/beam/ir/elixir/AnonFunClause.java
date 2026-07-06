472d7f93f190553fcbf6e1ef57c4b07a1af7dd7a
Frank Eickhoff <info@frankeickhoff.de>
2026-07-06 14:19:42 +0200

    feat(elixir): add optional guard to AnonFunClause

--- src/main/java/io/beam/ir/elixir/AnonFunClause.java
+++ src/main/java/io/beam/ir/elixir/AnonFunClause.java
@@ -2,5 +2,14 @@ package io.beam.ir.elixir;
 
 import java.util.List;
 
-public record AnonFunClause(List<Pattern> params, Expression body, SourceSpan source)
-    implements Node {}
+public record AnonFunClause(
+    List<Pattern> params, Guard guardOrNull, Expression body, SourceSpan source) implements Node {
+
+  public static AnonFunClause of(List<Pattern> params, Expression body) {
+    return new AnonFunClause(params, null, body, null);
+  }
+
+  public static AnonFunClause of(List<Pattern> params, Guard guard, Expression body) {
+    return new AnonFunClause(params, guard, body, null);
+  }
+}
